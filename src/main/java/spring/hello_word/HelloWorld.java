3584723739f75835ba3add2e353b276adbc30aec
Eric Hall <ericmbf@gmail.com>
2019-04-27 09:34:28 -0300

    Initial commit

---
+++ src/main/java/spring/hello_word/HelloWorld.java
@@ -0,0 +1,12 @@
+package spring.hello_word;
+
+public class HelloWorld {
+    private String message;
+ 
+    public void setMessage(String message){
+       this.message  = message;
+    }
+    public void getMessage(){
+       System.out.println("Your Message : " + message);
+    }
+ }
